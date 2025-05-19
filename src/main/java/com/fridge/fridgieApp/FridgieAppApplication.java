787bf404fd2b672b3d6f580720ebc7a695f6d3d4
Nikola Djordjevic <nikola.djordjevic013@gmail.com>
2025-05-19 23:26:42 +0200

    Initial commit

---
+++ src/main/java/com/fridge/fridgieApp/FridgieAppApplication.java
@@ -0,0 +1,13 @@
+package com.fridge.fridgieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgieAppApplication.class, args);
+	}
+
+}
